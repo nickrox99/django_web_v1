f2036c7c384a05bce232c6e95fc25cee79583f5b
nickrox99 <nick.cashiola@gmail.com>
2019-12-30 14:52:18 -0600

    initial commit [Nick Cashiola]

---
+++ ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/nicholascashiola/Documents/python/django_web_v1/ll_env/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
